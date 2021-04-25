507104d1c7f0270b33d6fc750d0bbdf84fd6e59b
Abhilash Jha <abhilashp.jha@outlook.com>
2021-04-26 00:06:40 +0530

    changes done by Abhilash first time

---
+++ Demo1/Demo1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Demo1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            this.utextbox.Clear();
+            this.upassword.Clear();
+        }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            if (utextbox.Text == "Admin" && upassword.Text == "admin123")
+            {
+                HomePage hmp = new HomePage();
+                hmp.Show();
+            }
+            else if(utextbox.Text == null || upassword.Text == null)
+            {
+                MessageBox.Show("Please enter user name password");
+            }
+            else if (utextbox.Text != "Admin" && upassword.Text != "admin123")
+            {
+                MessageBox.Show("please enter valid id password ");
+            }
+
+        }
+    }
+}
+
